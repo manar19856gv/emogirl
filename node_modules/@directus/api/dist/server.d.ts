84e2044c24ea9ebb8ce84e13da11f4e166c41c7e
Manar <a7bk19801980@gmail.com>
2025-06-01 13:39:54 +0200

    تنظيف المستودع وإضافة gitignore

--- node_modules/@directus/api/dist/server.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" resolution-mode="require"/>
-/// <reference types="node/http.js" />
-/// <reference types="pino-http" />
-import * as http from 'http';
-export declare let SERVER_ONLINE: boolean;
-export declare function createServer(): Promise<http.Server>;
-export declare function startServer(): Promise<void>;
